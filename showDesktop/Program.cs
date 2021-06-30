2310ac44612f96045d4385649f6dacaa334832ba
Manfred Mueller <mail@manfred-mueller.org>
2021-06-30 14:01:51 +0200

    Projektdateien hinzufügen.

---
+++ showDesktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace showDesktop
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Type typeShell = Type.GetTypeFromProgID("Shell.Application");
+            var objShell = Activator.CreateInstance(typeShell);
+
+            typeShell.InvokeMember("ToggleDesktop", System.Reflection.BindingFlags.InvokeMethod, null, objShell, null);
+        }
+    }
+}
